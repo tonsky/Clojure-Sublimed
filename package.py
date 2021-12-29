b9e9f36e3068473c6255f6e03dd0ce598434fe92
Jaihindh Reddy <jaihindhreddy@gmail.com>
2021-12-29 19:07:06 +0530

    Interrupt pending eval when region is erased

--- package.py
+++ package.py
@@ -201,6 +201,8 @@ class Connection:
         eval.erase()
         del self.evals[eval.id]
         del self.evals_by_view[eval.view.id()][eval.id]
+        if eval.status == "pending" and (session := eval.msg.get("session")):
+            conn.send({"op": "interrupt", "interrupt-id": eval.id, "session": session})
 
     def find_eval(self, view, region):
         for eval in self.evals_by_view[view.id()].values():
