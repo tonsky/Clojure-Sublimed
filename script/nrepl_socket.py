a08f3ee57529a99e8e4ad635f560740d59ef846e
Nikita Prokopov <niki@tonsky.me>
2022-11-16 20:56:12 +0100

    nrepl_socket.py

---
+++ script/nrepl_socket.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3
+import os, subprocess
+
+if __name__ == '__main__':
+  os.chdir(os.path.dirname(__file__) + "/..")
+  subprocess.check_call(['clojure',
+    '-Sdeps', '{:deps {nrepl/nrepl {:mvn/version "1.0.0"}}}',
+    '-M', '-m', 'nrepl.cmdline',
+    '--socket', 'nrepl-socket'
+  ])
