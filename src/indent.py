5382eeaa47ae974edb9b044223edaa182ae9955d
Nikita Prokopov <niki@tonsky.me>
2023-01-22 01:09:24 +0000

    Corrent indent on incomplete forms

--- src/indent.py
+++ src/indent.py
@@ -14,25 +14,42 @@ def indent(view, point):
     parsed = clojure_parser.parse_tree(view)
     if path := search_path(parsed, point):
         node = None
-        for n in reversed(path):
-            if n.name in ['string', 'parens', 'braces', 'brackets']:
-                node = n
+        first_form = None
+
+        # try finding unmatched open paren
+        for child in path[-1].children:
+            if child.start >= point:
                 break
-        # print(point, str(parsed), str(node))
+            if child.name == 'error' and child.text in ['(', '[', '{', '"']:
+                node = child
+                first_form = None
+            elif first_form is None:
+                first_form = child
+
+        # try indent relative to wrapping paren
+        if not node:
+            for n in reversed(path):
+                if n.name in ['string', 'parens', 'braces', 'brackets']:
+                    node = n
+                    first_form = node.body.children[0] if node.body and node.body.children else None
+                    break
+
+        # top level
         if not node:
             row, _ = view.rowcol(point)
             return ('top-level', row, 0)
-        row, col = view.rowcol(node.open.end)
+
+        row, col = view.rowcol(node.open.end if node.open else node.end)
         offset = 0
         if node.name == 'string':
             return ('string', row, col)
-        elif node.name == 'parens':
-            offset = 1
-            # special case for when first element in form is parens/braces/brackets
-            if body := node.body:
-                if body.children and (child := body.children[0]):
-                    if child.end <= point and child.name in ['parens', 'braces', 'brackets']:
-                        offset = 0
+        elif node.name == 'parens' or (node.name == 'error' and node.text == '('):
+            if not first_form:
+                offset = 0
+            elif first_form.end <= point and first_form.name in ['parens', 'braces', 'brackets']:
+                offset = 0
+            else:
+                offset = 1
         return ('indent', row, col + offset)
 
 def skip_spaces(view, point):
