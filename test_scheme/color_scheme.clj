fa4dd127e57deed8a064dca43eb4444e21194e16
Nikita Prokopov <niki@tonsky.me>
2024-08-20 14:51:15 +0200

    Clojure Sublimed color scheme

--- test_scheme/color_scheme.clj
+++ test_scheme/color_scheme.clj
@@ -1,12 +1,15 @@
 
 ; Constants
-nil true false \c \newline 123 1.23 10/20 :abc :abc.def/uvw.xyz #inst "1985-01-25"
+nil true false \c \tab 1 1.0 1/2 :a :a.b/c.d #inst "1985-01-25"
 
 ; Symbols
-abc ab/cd
+abc ab/cd _abc
 
 ; Strings
-"" "abc" "\" \u221e"
+"" "abc" "\" \u221e \x"
+
+; Regexps
+#"re \n \uFFFF \p{L} \Qabc\E) \y"
 
 ; Top-level parens
 () [] {} #() #{} #?() #?@() 
@@ -33,7 +36,122 @@ abc ab/cd
 ,
 
 ; Meta
+^{:s  sym
+  :v  "str\n"
+  :n  123
+  :re #"\p{L}) \y"
+  :l  ([{}])
+  :q  '(abc)
+  :sq `(abc ~def)
+  :m  ^int i
+  :d  @ref
+  :v  #'var
+  :c  #?(:clj)
+  :df (def x 1)
+  ; linecomment
+  #_#_reader comment
+  (comment form))))} sym
+
 ; Quotes
+'{:symbol  name/space
+  :string  "str\n\x"
+  :regexp  #"\p{L}) \y"
+  :number  123.456
+  :keyword :key/word
+  :parens  (() #() #?(:clj :cljs) [] {} #{})
+  :quoted  ('abc `(x ~y))
+  :meta    ^int i
+  :vars    [@ref #'var]
+  :defs    (def x 1)
+  ; linecomment
+  #_#_reader comment
+  (comment form))))}
+
 ; Syntax quotes
+`{:symbol  name/space
+  :string  "str\n\x"
+  :regexp  #"\p{L}) \y"
+  :number  123.456
+  :keyword :key/word
+  :parens  (() #() #?(:clj :cljs) [] {} #{})
+  :quoted  ('abc `(x ~y))
+  :meta    ^int i
+  :vars    [@ref #'var]
+  :defs    (def x 1)
+  ; linecomment
+  #_#_reader comment
+  (comment form))))}
+
+`{:symbol  name/space
+  :string  "str\n\x"
+  :regexp  #"\p{L}) \y"
+  :number  123.456
+  :keyword :key/word
+  :parens  (() #() #?(:clj :cljs) [] {} #{})
+  :quoted  ('abc `(x ~y))
+  :meta    ^int i
+  :vars    [@ref #'var]
+  :defs    (def x 1)
+  ; linecomment
+  #_#_reader comment
+  (comment form))))}
+
+`~{:symbol  name/space
+   :string  "str\n\x"
+   :regexp  #"\p{L}) \y"
+   :number  123.456
+   :keyword :key/word
+   :parens  (() #() #?(:clj :cljs) [] {} #{})
+   :quoted  ('abc `(x ~y))
+   :meta    ^int i
+   :vars    [@ref #'var]
+   :defs    (def x 1)
+   ; linecomment
+   #_#_reader comment
+   (comment form))))}
+
+; Line comments
+; {:symbol  name/space
+;  :string  "str\n\x"
+;  :regexp  #"\p{L}) \y"
+;  :number  123.456
+;  :keyword :key/word
+;  :parens  (() #() #?(:clj :cljs) [] {} #{})
+;  :quoted  ('abc `(x ~y))
+;  :meta    ^int i
+;  :vars    [@ref #'var]
+;  :defs    (def x 1)
+;  ; linecomment
+;  #_#_reader comment
+;  (comment form))))}
+
 ; Reader comments
+#_{:symbol  name/space
+   :string  "str\n\x"
+   :regexp  #"\p{L}) \y"
+   :number  123.456
+   :keyword :key/word
+   :parens  (() #() #?(:clj :cljs) [] {} #{})
+   :quoted  ('abc `(x ~y))
+   :meta    ^int i
+   :vars    [@ref #'var]
+   :defs    (def x 1)
+   ; linecomment
+   #_#_reader comment
+   (comment form))))}
+
 ; Form comments
+(comment
+  {:symbol  name/space
+   :string  "str\n\x"
+   :regexp  #"\p{L}) \y"
+   :number  123.456
+   :keyword :key/word
+   :parens  (() #() #?(:clj :cljs) [] {} #{})
+   :quoted  ('abc `(x ~y))
+   :meta    ^int i
+   :vars    [@ref #'var]
+   :defs    (def x 1)
+   ; linecomment
+   #_#_reader comment
+   (comment form))))})
